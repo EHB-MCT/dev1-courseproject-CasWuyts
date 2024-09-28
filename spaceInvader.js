9c6711e3938fadbca7ae5bbf4eef5f1052853873
Cas Wuyts <cas.wuyts@student.ehb.be>
2024-09-28 21:39:50 +0200

    SpaceInvade made

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,16 @@
-"use strict";
+let canvas = document.querySelector("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext("2d");
 
-alert("Draw your space invader here");
+signature()
+
+function signature(){
+    context.fillRect(50,50,300,300);
+    context.fillStyle = '#0EFFF8';
+    context.fillRect(75,75,50,50);
+    context.fillRect(275,75,50,50)
+    context.fillRect(75,225,250,50)
+    context.fillRect(125,175,50,50)
+    context.fillRect(225,175,50,50)
+}
