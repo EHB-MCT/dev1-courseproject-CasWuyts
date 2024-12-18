17561e68fedda3858bd7b4588c558b31b1d7fb6e
Cas Wuyts <cas.wuyts@student.ehb.be>
2024-12-18 12:03:48 +0100

    zonder animatie

--- opdrachten/3-Interactive-design.js
+++ opdrachten/3-Interactive-design.js
@@ -6,6 +6,7 @@ let width = canvas.width;
 let height = canvas.height;
 let radius = Utils.randomNumber(300, 400);
 let sunDrawn = false;
+let windowLightness = 50;
 
 draw();
 
@@ -24,21 +25,42 @@ function draw() {
 }
 
 function drawSkyline() {
-	for (let i = 0; i < 20; i++) {
+	for (let i = 0; i < 40; i++) {
 		let y = Utils.randomNumber(height - 150, height - 500);
 		let buildingWidth = Utils.randomNumber(width / 30, width / 10);
 
 		let lightness;
 		if (sunDrawn == false) {
 			lightness = Utils.randomNumber(1, 10);
-		} else if ((sunDrawn = true)) {
-			lightness = Utils.randomNumber(30, 50);
-		} else if (radius < 350) {
-			lightness = Utils.randomNumber(15, 25);
+		} else {
+			lightness = Utils.randomNumber(20, 30);
 		}
 
 		context.fillStyle = Utils.hsl(Utils.randomNumber(150, 265), 100, lightness);
-		context.fillRect(100 * i + 50, y, buildingWidth, height);
+		context.fillRect(50 * i + 50, y, buildingWidth, height);
+
+		drawWindows(50 * i + 50, y, buildingWidth, height - y);
+	}
+}
+
+function drawWindows(buildingX, buildingY, buildingWidth, buildingHeight) {
+	// Bereken het aantal rijen en kolommen ramen
+	let rows = Math.floor(buildingHeight / 20);
+	let cols = Math.floor(buildingWidth / 15);
+
+	for (let row = 0; row < rows; row++) {
+		for (let col = 0; col < cols; col++) {
+			let x = buildingX + col * 15 + 5;
+			let y = buildingY + row * 20 + 5;
+
+			if (sunDrawn == false) {
+				windowLightness = context.fillStyle = Utils.hsl(50, 100, 50);
+			} else {
+				windowLightness = context.fillStyle = Utils.hsl(50, 100, 80);
+			}
+
+			context.fillRect(x, y, 10, 10); // Ramen van 10x10 pixels
+		}
 	}
 }
 
